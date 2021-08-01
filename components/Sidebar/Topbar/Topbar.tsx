780aa3556d2fd9ea0767bbe2057a610039b0d37a
Pranav Doshi <pranav.rdoshi@gmail.com>
2021-08-01 17:34:09 +0530

    fix: topbar icons

--- components/Sidebar/Topbar/Topbar.tsx
+++ components/Sidebar/Topbar/Topbar.tsx
@@ -2,7 +2,14 @@
 import styles from './Topbar.module.css'
 
 const Topbar = () => {
-  return <div className={styles.topbar}></div>
+  return (
+    <div className={styles.topbar}>
+      <img src="/icons/folder_blue.svg" alt="Files" />
+      <img src="/icons/search.svg" alt="Search" />
+      <img src="/icons/git.svg" alt="Git" />
+      <img src="/icons/extension.svg" alt="Extensions" />
+    </div>
+  )
 }
 
 export default Topbar
